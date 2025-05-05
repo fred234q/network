f4dec8052952a77f561ed7870c183f38965b1688
Frederik From <frederik@localhost.localdomain>
2025-05-06 00:18:44 +0200

    First try at create post API.

--- network/models.py
+++ network/models.py
@@ -3,4 +3,14 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField('User', related_name="followers")
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    timestamp = models.DateTimeField(auto_now_add=True)
